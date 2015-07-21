11f09c23fb821bdf87a250d0ec1c1915e4a676af
Bård Hovde <bard@cddnation.com>
2015-07-21 15:07:58 +0100

    FIX - cocked that up..

--- app/index.js
+++ app/index.js
@@ -143,6 +143,7 @@ module.exports = yeoman.generators.Base.extend({
         copyApp: function() {
 
             if ( this.appType !== 'perch' ) {
+                this.mkdir('app/perch');
                 this.directory('perch', 'app/perch');
                 this.fs.copyTpl(
                     this.templatePath('_index.html'),
